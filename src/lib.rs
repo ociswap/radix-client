e8ca25f65c80b2a81189d0fd0e17a92f1ffc4ce3
Krulknul <85589076+Krulknul@users.noreply.github.com>
2024-03-21 16:12:12 +0100

    make core and gateway optional features each

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 pub mod constants;
+#[cfg(feature = "core")]
 pub mod core;
+#[cfg(feature = "gateway")]
 pub mod gateway;
 
 use duplicate::duplicate_item;
@@ -7,10 +9,22 @@ use maybe_async::{must_be_async, must_be_sync};
 use reqwest;
 use serde::Serialize;
 
+#[cfg(feature = "gateway")]
 #[duplicate_item(
     client_type                 reqwest_client_type ;
     [ GatewayClientAsync ]     [ reqwest::Client ];
     [ GatewayClientBlocking ]  [ reqwest::blocking::Client ];
+
+  )]
+#[derive(Debug, Clone)]
+pub struct client_type {
+    pub base_url: String,
+    pub client: reqwest_client_type,
+}
+
+#[cfg(feature = "core")]
+#[duplicate_item(
+    client_type                 reqwest_client_type ;
     [ CoreClientAsync ]        [ reqwest::Client ];
     [ CoreClientBlocking ]     [ reqwest::blocking::Client ];
   )]
@@ -20,13 +34,47 @@ pub struct client_type {
     pub client: reqwest_client_type,
 }
 
+#[cfg(feature = "gateway")]
 #[duplicate_item(
     client_type                 reqwest_client_type           maybe_async_attr;
     [ GatewayClientAsync ]     [ reqwest::Client ]          [ must_be_async ];
     [ GatewayClientBlocking ]  [ reqwest::blocking::Client ] [ must_be_sync ];
+  )]
+impl client_type {
+    pub fn new(base_url: String) -> client_type {
+        client_type {
+            base_url,
+            client: reqwest_client_type::new(),
+        }
+    }
+
+    #[maybe_async_attr]
+    pub async fn post_request<S: Serialize>(
+        &self,
+        path: &str,
+        body: S,
+    ) -> Result<(String, reqwest::StatusCode), reqwest::Error> {
+        let res = self
+            .client
+            .post(format!("{}/{}", &self.base_url, path))
+            .header(reqwest::header::ACCEPT, "application/json")
+            .header(reqwest::header::USER_AGENT, "reqwest/0.11.0")
+            .json(&body)
+            .send()
+            .await
+            .unwrap();
+        let status = res.status();
+        let text = res.text().await?;
+        Ok((text, status))
+    }
+}
+
+#[cfg(feature = "core")]
+#[duplicate_item(
+    client_type                 reqwest_client_type           maybe_async_attr;
     [ CoreClientAsync ]        [ reqwest::Client ]          [ must_be_async ];
     [ CoreClientBlocking ]     [ reqwest::blocking::Client ] [ must_be_sync ];
-  )]
+)]
 impl client_type {
     pub fn new(base_url: String) -> client_type {
         client_type {
